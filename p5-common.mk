7e378e6e1fc39a9c9b20541eb8b020e909c79c06
kallt_kaffe <kallt_kaffe@apedroid.com>
2012-03-07 20:55:07 +0100

    p5-common: Use bcmdhd
    Change-Id: I61f1578d4c5aa0cc4112caa6609327475060932c
    Reference: p4-common

--- p5-common.mk
+++ p5-common.mk
@@ -70,6 +70,13 @@ PRODUCT_PROPERTY_OVERRIDES := \
     wifi.interface=eth0 \
     wifi.supplicant_scan_interval=15
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/bcm4330_aps.bin:system/etc/wifi/bcm4330_apsta.bin
+
+BOARD_WLAN_DEVICE_REV := bcm4330_b1
+
+$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
+
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
         persist.sys.usb.config=mtp
